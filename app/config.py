ea505abdde30c7ce7104509505564785f80c3a8b
Enescu <adelin_enescu@yahoo.com>
2025-12-16 18:31:19 +0200

    feat: add auth related variablees to .env

--- app/config.py
+++ app/config.py
@@ -1,1 +1,8 @@
-HORIZONS_URL = "https://ssd.jpl.nasa.gov/api/horizons.api"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
