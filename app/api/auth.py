316e6391ecb2a719b92c972b39b0870a174a468c
Enescu <adelin_enescu@yahoo.com>
2025-12-17 13:46:42 +0200

    refactor: move FastAPI app into main.py

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, status
+from app.schemas.auth import UserIn, UserOut, Token 
+import app.services.auth as auth
+
+auth_router = APIRouter(prefix="/auth")
+
+@auth_router.post("/register", response_model=UserOut, status_code=status.HTTP_201_CREATED)
+def register_user(user_in: UserIn)
 
 
-app = FastAPI(title="SkyArchive")
-admin_app = FastAPI()
 
 
-@app.get("/")
-def health_check():
-    return {"status": "running"}
