1e567914d5c3436c34e9611efc376f79e3ddacb8
Enescu <adelin_enescu@yahoo.com>
2025-12-19 20:36:55 +0200

    refcator: add pytest fixture to roll back db after each test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,7 +1,7 @@
 from app.main import app
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 from app.db.base import Base
 from app.db.session import get_session
 from app.services.auth import get_user_by_username, verify_password, create_user
@@ -12,26 +12,35 @@ client = TestClient(app)
 
 fake_engine = create_engine("sqlite:///:memory:", connect_args={"check_same_thread": False})
 
-connection = fake_engine.connect()
 
-
-SessionLocal = sessionmaker(bind=connection)
-
-Base.metadata.create_all(bind=connection)
+Base.metadata.create_all(bind=fake_engine)
 
 
 
 @pytest.fixture
-def override_get_session():
+def override_get_session(db_session):
+    def override_dependency():
+        return db_session
+    
     app.dependency_overrides[get_session] = override_dependency
     yield
     app.dependency_overrides.clear()
 
-def override_dependency():
-    return SessionLocal()
+
+@pytest.fixture
+def db_session():
+    connection = fake_engine.connect()
+    transaction = connection.begin()
+    SessionLocal = sessionmaker(bind=connection)
+    session = SessionLocal()
+    yield session
+    session.close()
+    transaction.rollback()
+    transaction.close()
 
 
-def test_register_success(override_get_session):
+
+def test_register_success(override_get_session, db_session):
 
     response = client.post("/auth/register",
                 json={
@@ -39,7 +48,7 @@ def test_register_success(override_get_session):
                     "password": "testtest123"
                 }
     )
-    user = get_user_by_username("Thisisatest", override_dependency())
+    user = get_user_by_username("Thisisatest", db_session)
 
     assert response.status_code == 201
     assert user is not None
@@ -48,9 +57,9 @@ def test_register_success(override_get_session):
 
 
 
-def test_register_duplicate_username(override_get_session):
+def test_register_duplicate_username(override_get_session, db_session):
 
-    create_user("yohellothere", "justatest", override_dependency())
+    create_user("yohellothere", "justatest", db_session)
 
 
     response = client.post(
@@ -65,10 +74,10 @@ def test_register_duplicate_username(override_get_session):
     assert response.json() == {"detail": "Username already exists"}
 
 
-def test_login_successful(override_get_session):
+def test_login_successful(override_get_session, db_session):
     
 
-    create_user("myuser", "mypassword", override_dependency())
+    create_user("myuser", "mypassword", db_session)
 
     response = client.post(
                 "/auth/login",
@@ -83,9 +92,9 @@ def test_login_successful(override_get_session):
     assert "access_token" in response.json()
 
 
-def test_login_invalid_credentials(override_get_session):
+def test_login_invalid_credentials(override_get_session, db_session):
 
-    create_user("hithere", "byethere", override_dependency())
+    create_user("hithere", "byethere", db_session)
 
     response = client.post(
                 "auth/login",
