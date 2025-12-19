76372dd6a0881a336f31c1f37dba417963c34c1a
Enescu <adelin_enescu@yahoo.com>
2025-12-19 20:02:48 +0200

    refactor: add pytest fixture to reduce repetitive code

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -5,8 +5,8 @@ from sqlalchemy.orm import sessionmaker
 from app.db.base import Base
 from app.db.session import get_session
 from app.services.auth import get_user_by_username, verify_password, create_user
-from app.services.auth import create_access_token
-import json
+import pytest
+
 
 client = TestClient(app)
 
@@ -20,12 +20,18 @@ SessionLocal = sessionmaker(bind=connection)
 Base.metadata.create_all(bind=connection)
 
 
+
+@pytest.fixture
+def override_get_session():
+    app.dependency_overrides[get_session] = override_dependency
+    yield
+    app.dependency_overrides.clear()
+
 def override_dependency():
     return SessionLocal()
 
 
-def test_register_success():
-    app.dependency_overrides[get_session] = override_dependency
+def test_register_success(override_get_session):
 
     response = client.post("/auth/register",
                 json={
@@ -34,17 +40,15 @@ def test_register_success():
                 }
     )
     user = get_user_by_username("Thisisatest", override_dependency())
-    try:
-        assert response.status_code == 201
-        assert user is not None
-        assert user.username == "Thisisatest"
-        assert verify_password("testtest123", user.hashed_password) == True
-    finally:
-        app.dependency_overrides.clear()
+
+    assert response.status_code == 201
+    assert user is not None
+    assert user.username == "Thisisatest"
+    assert verify_password("testtest123", user.hashed_password) == True
 
 
-def test_register_duplicate_username():
-    app.dependency_overrides[get_session] = override_dependency
+
+def test_register_duplicate_username(override_get_session):
 
     create_user("yohellothere", "justatest", override_dependency())
 
@@ -56,35 +60,30 @@ def test_register_duplicate_username():
                     "password": "hihowareyou"
                 }
     )
-    try:
-        assert response.status_code == 409
-        assert response.json() == {"detail": "Username already exists"}
-    finally:
-        app.dependency_overrides.clear()
+   
+    assert response.status_code == 409
+    assert response.json() == {"detail": "Username already exists"}
 
 
-def test_login_successful():
-    app.dependency_overrides[get_session] = override_dependency
+def test_login_successful(override_get_session):
+    
 
     create_user("myuser", "mypassword", override_dependency())
 
     response = client.post(
-                "auth/login",
+                "/auth/login",
                 json={
                     "username": "myuser",
                     "password": "mypassword",
                 }
     )
 
-    try:
-        assert response.status_code == 200
-        assert "access_token" in response.json()
-    finally:
-        app.dependency_overrides.clear()
+
+    assert response.status_code == 200
+    assert "access_token" in response.json()
 
 
-def test_login_invalid_credentials():
-    app.dependency_overrides[get_session] = override_dependency
+def test_login_invalid_credentials(override_get_session):
 
     create_user("hithere", "byethere", override_dependency())
 
@@ -96,8 +95,6 @@ def test_login_invalid_credentials():
                 }
     )
 
-    try:
-        assert response.status_code == 401
-        assert response.json() == {"detail": "Invalid credentials"}
-    finally:
-        app.dependency_overrides.clear()
+
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Invalid credentials"}
