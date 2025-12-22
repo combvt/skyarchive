aaf2e07b823a30acaad4c27e93f1cad87bad4fe7
Enescu <adelin_enescu@yahoo.com>
2025-12-22 20:01:16 +0200

    feat: implement Horizons ephemeris data parsing

--- app/services/horizons.py
+++ app/services/horizons.py
@@ -45,3 +45,69 @@ def search_object(object_name: str | int, coords: str) -> dict:
     return data
 
 
+data = search_object("1:", "51.5,0.1,0.005")
+
+def parse_horizons_ephemeris(raw_data: dict) -> dict:
+    data_dict = {}
+    data_dict["source"] = raw_data.get("signature", {}).get("source", "Unknown source")
+
+
+    data = raw_data["result"]
+    start_index = data.find("$$SOE") + 5
+    end_index = data.find("$$EOE")
+
+    name_start_index = data.find("Target body name:")
+    name_end_index = data.find(r"{source:")
+
+
+
+    if data.find("No matches found.") != -1:
+        raise ObjectNotFoundError
+    elif data.find("Number of matches =") != -1 or data.find("Matching small-bodies:") != -1:
+              return data
+    elif start_index != -1 and end_index != -1:
+        name_id_string = data[name_start_index:name_end_index].strip()
+        first_slice_index = name_id_string.find("(")
+        second_slice_index = name_id_string.find(")")
+        object_name = name_id_string[len("Target body name:"):first_slice_index].strip()
+        object_id = name_id_string[first_slice_index + 1:second_slice_index].strip()
+        
+        data_dict["object_name"] = object_name
+        data_dict["object_id"] = object_id
+
+        sliced_string = data[start_index:end_index].strip()
+
+        clean_data = sliced_string.splitlines()[0].replace("*m", "")
+        parsed_string = clean_data.replace("/T", "").replace("/L", "").split()
+
+        i = 0
+        data_dict["date"] = f"{parsed_string[i]} {parsed_string[i + 1]}"
+        i += 16
+        data_dict["azimuth_deg"] = parsed_string[i]
+        i += 1
+        data_dict["altitude_deg"] = parsed_string[i]
+        i += 11
+        data_dict["apparent_magnitude"] = parsed_string[i]
+        i += 1
+        data_dict["surface_brightness"] = parsed_string[i]
+        i += 1
+        data_dict["illumination_percent"] = parsed_string[i]
+        i += 3
+        data_dict["angular_diameter_arcsec"] = parsed_string[i]
+        i += 11
+        data_dict["sun_distance_au"] = parsed_string[i]
+        i += 2
+        data_dict["earth_distance_au"] = parsed_string[i]
+        i += 6
+        data_dict["solar_elong_deg"] = parsed_string[i]
+        i += 7
+        data_dict["constellation"] = parsed_string[i]
+
+        return data_dict
+    else:
+        raise UpstreamServiceError
+    
+
+
+
+print(parse_horizons_ephemeris(data))
