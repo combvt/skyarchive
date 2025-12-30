5dee6887b0ce1eb4b95465f71a539d9ce08a771a
Enescu <adelin_enescu@yahoo.com>
2025-12-30 14:29:46 +0200

    chore: format tests with black

--- tests/test_horizons.py
+++ tests/test_horizons.py
@@ -9,6 +9,7 @@ from app.services.auth import create_user
 import pytest
 from app.exceptions import ObjectNotFoundError, EphemerisDataMissing
 from app.exceptions import InvalidLocationError, UpstreamServiceError
+
 client = TestClient(app)
 
 fake_engine = create_engine(
@@ -16,6 +17,7 @@ fake_engine = create_engine(
 )
 
 Base.metadata.create_all(bind=fake_engine)
+
 
 @pytest.fixture
 def override_get_session(db_session):
@@ -25,6 +27,7 @@ def override_get_session(db_session):
     app.dependency_overrides[get_session] = override_dependency
     yield
     app.dependency_overrides.clear()
+
 
 @pytest.fixture
 def db_session():
@@ -37,54 +40,61 @@ def db_session():
     transaction.rollback()
     transaction.close()
 
+
 @pytest.fixture
 def test_user(db_session):
-    user = create_user("testing","fortest", db_session)
-    return {
-        "username": user.username,
-        "password": "fortest"
-    }
+    user = create_user("testing", "fortest", db_session)
+    return {"username": user.username, "password": "fortest"}
+
 
 @pytest.fixture
 def auth_header(test_user, override_get_session):
     user = test_user
-    response = client.post("/auth/login", data={
-        "username": user["username"],
-        "password": user["password"]
-    })
+    response = client.post(
+        "/auth/login", data={"username": user["username"], "password": user["password"]}
+    )
     token = response.json()["access_token"]
 
     assert response.status_code == 200
 
     return {"Authorization": f"Bearer {token}"}
 
+
 @pytest.fixture
 def mock_search_object(monkeypatch):
     def fake_search_object(object_name: str | int, coords: str):
-        return {"source": "fake", "result": "Ephemeris data for x object \n\n 2025-Dec-25 15:58 21.2 36.4 213231"}
+        return {
+            "source": "fake",
+            "result": "Ephemeris data for x object \n\n 2025-Dec-25 15:58 21.2 36.4 213231",
+        }
+
     monkeypatch.setattr("app.api.horizons.search_object", fake_search_object)
+
 
 @pytest.fixture
 def mock_get_coords(monkeypatch):
     def fake_get_coords(location: str, elevation: str | None = None):
         return "21.6,55,0.3"
+
     monkeypatch.setattr("app.api.horizons.get_coords", fake_get_coords)
 
+
 def test_horizons_search_requires_auth(override_get_session):
-    
-    response = client.get(url="/horizons/search", headers={
-        "Authorization": "Bearer thisisnotatoken213132"
-    },
-    params={
-        "query": "mars",
-        "location": "prague"
-    })
+
+    response = client.get(
+        url="/horizons/search",
+        headers={"Authorization": "Bearer thisisnotatoken213132"},
+        params={"query": "mars", "location": "prague"},
+    )
 
     assert response.status_code == 401
     assert response.json() == {"detail": "Could not validate credentials"}
 
-def test_horizons_search_returns_single_ephemeris(override_get_session, auth_header, monkeypatch, mock_search_object, mock_get_coords):
-    
+
+def test_horizons_search_returns_single_ephemeris(
+    override_get_session, auth_header, monkeypatch, mock_search_object, mock_get_coords
+):
+
     def fake_parse_horizons_ephemeris(raw_data: dict):
         return {
             "object_name": "Mars",
@@ -99,16 +109,18 @@ def test_horizons_search_returns_single_ephemeris(override_get_session, auth_hea
             "sun_distance_au": 1.431151111878,
             "earth_distance_au": 2.41248972367116,
             "solar_elong_deg": 1.9588,
-            "constellation": "Sgr"
+            "constellation": "Sgr",
         }
-    
-    monkeypatch.setattr("app.api.horizons.parse_horizons_ephemeris", fake_parse_horizons_ephemeris)
 
-    response = client.get("/horizons/search", params={
-        "query": 499,
-        "location": "london"
-    },
-    headers=auth_header)
+    monkeypatch.setattr(
+        "app.api.horizons.parse_horizons_ephemeris", fake_parse_horizons_ephemeris
+    )
+
+    response = client.get(
+        "/horizons/search",
+        params={"query": 499, "location": "london"},
+        headers=auth_header,
+    )
     assert response.status_code == 200
     data = response.json()
     assert isinstance(data, dict)
@@ -119,36 +131,43 @@ def test_horizons_search_returns_single_ephemeris(override_get_session, auth_hea
     assert data["constellation"] == "Sgr"
 
 
-def test_horizons_search_multi_match_response(override_get_session, mock_search_object, mock_get_coords, auth_header, monkeypatch):
+def test_horizons_search_multi_match_response(
+    override_get_session, mock_search_object, mock_get_coords, auth_header, monkeypatch
+):
     def fake_parse_horizons_ephemeris(raw_data: dict):
         return [
             {
                 "object_name": "Mars Barycenter",
                 "object_id": "4",
                 "designation": None,
-                "aliases": None
+                "aliases": None,
             },
             {
                 "object_name": "Mars",
                 "object_id": "499",
                 "designation": None,
-                "aliases": None
+                "aliases": None,
             },
             {
                 "object_name": "Mars Orbiter Mission (spacecraft)",
                 "object_id": "-3",
                 "designation": "2013-060A",
-                "aliases": "MOM Mangalyaan"
+                "aliases": "MOM Mangalyaan",
             },
         ]
-    
-    monkeypatch.setattr("app.api.horizons.parse_horizons_ephemeris", fake_parse_horizons_ephemeris)
 
-    response = client.get("/horizons/search", params={
-        "query": "mars",
-        "location": "Honolulu",
-    },
-    headers=auth_header)
+    monkeypatch.setattr(
+        "app.api.horizons.parse_horizons_ephemeris", fake_parse_horizons_ephemeris
+    )
+
+    response = client.get(
+        "/horizons/search",
+        params={
+            "query": "mars",
+            "location": "Honolulu",
+        },
+        headers=auth_header,
+    )
 
     assert response.status_code == 200
     data = response.json()
@@ -158,66 +177,97 @@ def test_horizons_search_multi_match_response(override_get_session, mock_search_
     assert "designation" not in data[0]
     assert data[1]["object_name"] == "Mars"
     assert "aliases" in data[2]
-    
-def test_horizons_search_object_not_found_returns_404(override_get_session, auth_header, monkeypatch, mock_search_object, mock_get_coords):
+
+
+def test_horizons_search_object_not_found_returns_404(
+    override_get_session, auth_header, monkeypatch, mock_search_object, mock_get_coords
+):
     def fake_parse_horizons_ephemeris(raw_data: dict):
         raise ObjectNotFoundError
-    
-    monkeypatch.setattr("app.api.horizons.parse_horizons_ephemeris", fake_parse_horizons_ephemeris)
 
-    response = client.get("/horizons/search", params={
-        "query": "mars",
-        "location": "Honolulu",
-    },
-    headers=auth_header)
+    monkeypatch.setattr(
+        "app.api.horizons.parse_horizons_ephemeris", fake_parse_horizons_ephemeris
+    )
+
+    response = client.get(
+        "/horizons/search",
+        params={
+            "query": "mars",
+            "location": "Honolulu",
+        },
+        headers=auth_header,
+    )
 
     assert response.status_code == 404
     assert response.json() == {"detail": "Object not found"}
 
-def test_horizons_search_ephemeris_data_missing_returns_404(override_get_session, auth_header, monkeypatch, mock_search_object, mock_get_coords):
+
+def test_horizons_search_ephemeris_data_missing_returns_404(
+    override_get_session, auth_header, monkeypatch, mock_search_object, mock_get_coords
+):
     def fake_parse_horizons_ephemeris(raw_data: dict):
         raise EphemerisDataMissing
-    
-    monkeypatch.setattr("app.api.horizons.parse_horizons_ephemeris", fake_parse_horizons_ephemeris)
 
-    response = client.get("/horizons/search", params={
-        "query": "mars",
-        "location": "Honolulu",
-    },
-    headers=auth_header)
+    monkeypatch.setattr(
+        "app.api.horizons.parse_horizons_ephemeris", fake_parse_horizons_ephemeris
+    )
+
+    response = client.get(
+        "/horizons/search",
+        params={
+            "query": "mars",
+            "location": "Honolulu",
+        },
+        headers=auth_header,
+    )
 
     assert response.status_code == 404
     assert response.json() == {"detail": "No ephemeris data available for this object"}
 
-def test_horizons_search_upstream_service_error_returns_503(override_get_session, auth_header, monkeypatch, mock_search_object, mock_get_coords):
+
+def test_horizons_search_upstream_service_error_returns_503(
+    override_get_session, auth_header, monkeypatch, mock_search_object, mock_get_coords
+):
     def fake_parse_horizons_ephemeris(raw_data: dict):
         raise UpstreamServiceError
-    
-    monkeypatch.setattr("app.api.horizons.parse_horizons_ephemeris", fake_parse_horizons_ephemeris)
 
-    response = client.get("/horizons/search", params={
-        "query": "mars",
-        "location": "Honolulu",
-    },
-    headers=auth_header)
+    monkeypatch.setattr(
+        "app.api.horizons.parse_horizons_ephemeris", fake_parse_horizons_ephemeris
+    )
+
+    response = client.get(
+        "/horizons/search",
+        params={
+            "query": "mars",
+            "location": "Honolulu",
+        },
+        headers=auth_header,
+    )
 
     assert response.status_code == 503
     assert response.json() == {"detail": "Upstream Horizons service error"}
 
-def test_horizons_search_invalid_location_returns_400(override_get_session, auth_header, monkeypatch):
+
+def test_horizons_search_invalid_location_returns_400(
+    override_get_session, auth_header, monkeypatch
+):
     def fake_get_coords(location: str, elevation: str | None = None):
         raise InvalidLocationError
-    
+
     monkeypatch.setattr("app.api.horizons.get_coords", fake_get_coords)
 
-    response = client.get("/horizons/search", params={
-        "query": "mars",
-        "location": "Honolulu",
-    },
-    headers=auth_header)
+    response = client.get(
+        "/horizons/search",
+        params={
+            "query": "mars",
+            "location": "Honolulu",
+        },
+        headers=auth_header,
+    )
 
     assert response.status_code == 400
     assert response.json() == {"detail": "Invalid location"}
+
 
 def test_single_match_ephemeris_parser():
     raw_data = {
@@ -228,7 +278,7 @@ def test_single_match_ephemeris_parser():
         $$EOE
     """
     }
-    
+
     data = parse_horizons_ephemeris(raw_data)
     assert isinstance(data, dict)
     assert data["object_name"] == "Mars"
@@ -239,5 +289,4 @@ def test_single_match_ephemeris_parser():
     assert data["angular_diameter_arcsec"] == "3.876377"
     assert "*m" not in data
 
-#TODO add test for single match parser, assert token count ==
-# len(data), correct slicing (give it a real string)
+
