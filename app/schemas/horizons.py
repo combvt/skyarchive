00a8765fc961d6ec2065f3f3c3cb327d6854e8f9
Enescu <adelin_enescu@yahoo.com>
2026-01-07 22:39:25 +0200

    feat: implement ephemeris data response model for POST endpoint

--- app/schemas/horizons.py
+++ app/schemas/horizons.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 
 
@@ -30,3 +31,22 @@ class EphemerisCreate(BaseModel):
     object_id : str | int
     location: str
     elevation: float | None = None
+
+
+class EphemerisOut(BaseModel):
+    id: int
+    object_name: str
+    object_id: str
+    azimuth_deg: float | None = None
+    altitude_deg: float | None = None
+    apparent_magnitude: float | None = None
+    surface_brightness: float | None = None
+    illumination_percent: float | None = None
+    angular_diameter_arcsec: float | None = None
+    sun_distance_au: float | None = None
+    earth_distance_au: float | None = None
+    solar_elong_deg: float | None = None
+    constellation: str | None = None
+    date: str
+    coords: str
+    created_at: datetime
